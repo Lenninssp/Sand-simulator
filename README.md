ef62678d690fe8728e3c3d60e8efb58883ad97a5
Lennin sabogal <lenninssp1021@gmail.com>
2024-02-03 14:54:31 -0500

    sand simulator working

--- README.md
+++ README.md
@@ -23,14 +23,38 @@ A 2D Canvas will work best with our falling sand simulator.
 
 if one block is equal to 1 and the block below is wqual to 0 then they will swap places:
 
-| 0 | 0 | 0 | 0 | 0 | 0 | ➡️  | 0 | 0 | 0 | 0 | 0 | 0 |
+| 0 | 0 | 0 | 0 | 0 | 0 | ➡️ | 0 | 0 | 0 | 0 | 0 | 0 |
 |---|---|---|---|---|---|---|---|---|---|---|---|---|
-| 0 | 0 | 0 | 1️⃣ | 0 | 0 | ➡️  | 0 | 0 | 0 | 0️⃣ | 0 | 0 |
-| 0 | 0 | 0 | 0️⃣ | 0 | 0 |  ➡️ | 0 | 0 | 0 | 1️⃣ | 0 | 0 |
+| 0 | 0 | 0 | 1️⃣ | 0 | 0 | ➡️ | 0 | 0 | 0 | 0️⃣ | 0 | 0 |
+| 0 | 0 | 0 | 0️⃣ | 0 | 0 | ➡️ | 0 | 0 | 0 | 1️⃣ | 0 | 0 |
 | 0 | 0 | 0 | 0 | 0 | 0 |  ➡️ | 0 | 0 | 0 | 0 | 0 | 0 |
 | 0 | 0 | 0 | 0 | 0 | 0 |  ➡️ | 0 | 0 | 0 | 0 | 0 | 0 |
 
 
 >if there is already a 1 under then nothing happens
 
+
+---
+Right here the code works great, but this physics are really basic, so lets search in the possibility of making it more complex by applying more realistic physics
+
+---
+
+## Getting physical
+
+
+| 0 | 0 | 0 | 0 | 0 | 0 |
+|---|---|---|---|---|---|
+| 0 | 0 | 0 | 1 | 0 | 0 |
+| 0 | 0 | 0 | 1 | 0 | 0 |
+| 0 | 0 | 0 | 1 | 0 | 0 |
+| 0 | 0 | 0 | 1 | 0 | 0 |
+
+If it encounters a 1 below but there is a 0 in the below sides it may fall to the left or the right
+
+| 0 | 0 | 0 | 0 | 0 | 0 |
+|---|---|---|---|---|---|
+| 0 | 0 | 0 | 0 | 0 | 0 |
+| 0 | 0 | 0 | 1 | 0 | 0 |
+| 0 | 0 | 0 | 1 | 0 | 0 |
+| 0 | 0 | 1 | 1 | 0 | 0 |
 
--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 //code made by Lennin Sabogal based on The Code Train code
 
-function make2DArray(cols, rows){
+//in this function i create a 2D array in which i can store the values of each box in the canva
+function make2DArray(cols, rows){ //it will be used to create the array the values of columns and rows
     let arr = new Array(cols);
     for(let i = 0; i < arr.length ; i ++){
         arr[i] = new Array(rows)
+        for ( let j = 0; j < arr[i].length ; j++){
+            arr[i][j] = 0;
+        }
     }
     return arr;
 }
@@ -13,7 +17,7 @@ let w = 10;
 let cols, rows;
 
 function setup() {
-    createCanvas(400,400);
+    createCanvas(1080,720);
     cols = width/w;
     rows = height/w;
     grid = make2DArray(cols, rows); 
@@ -26,28 +30,64 @@ function setup() {
 
 }
 
+function mouseDragged(){
+    let mouseCol = floor(mouseX / w);
+    let mouseRow = floor(mouseY / w);
+    let matrix = 2;
+    let extent = floor(matrix/2);
+    for (let i = -extent; i <= extent; i++) {
+        for (let j = -extent; j <= extent; j++) {
+            if (random(1) < 0.75){
+                col = mouseCol + i;
+                row = mouseRow + j;
+                if (col >= 0 && col <= cols-1 && row>=0 &&row<=rows-1) {
+                    grid[col][row] = 1;
+              }
+            }
+        }
+    }
+    grid[col][row] = 1;
+    
+}
+
 function draw() {
     background(0);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            stroke(255);
-            fill(grid[i][j] * 255);
-            let x = i * w;
-            let y = j * w;
-            square(x, y, w);
+            noStroke(0);
+            if(grid[i][j] == 1){
+                fill(grid[i][j] * 255);
+                let x = i * w;
+                let y = j * w;
+                square(x, y, w);
+            }
         }
     }
 
-    grid[10][10] = 1;
     let nextgrid = make2DArray(cols, rows);
     for (let i = 0; i < cols ; i++){
         for (let j = 0; j < rows; j++){
             let state = grid[i][j];
-            if(state === 1){
+            if(state === 1 ){
                 let below = grid[i][j+1];
+                let dir = random([-1,1]);
+                let belowA, belowB;
+                
+                if ( i > 0 && i<cols-1){
+                    belowA = grid[i+dir][j+1];
+                    belowB = grid[i-dir][j+1];
+                }
                 if (below === 0){
-                    nextgrid[i][j] = 0;
-                    nextgrid[i][+1] = 1;
+                    nextgrid[i][j+1] = 1;
+                }
+                else if (belowA === 0 && i < cols -1) {
+                    nextgrid[i+1][j+1] = 1;
+                }
+                else if (belowB === 0 && i < cols -1) {
+                    nextgrid[i-1][j+1] = 1;
+                }
+                else {
+                    nextgrid[i][j] = 1;
                 }
             }
         }
